ef468f172769bb252809e5d0a8bb6afb05358bc8
“JZhixuan Tan <zhiuxan.tan@stonybrook.edu>
2022-05-19 06:25:56 -0400

    second commit

---
+++ app/src/main/java/com/example/remembertpass22/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.remembertpass22;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvContent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvContent = findViewById(R.id.tv_content);
+        Intent intent = getIntent();
+        String account = intent.getStringExtra("account");
+        tvContent.setText("Hello："+account);
+
+    }
+
+    public void logout(View view) {
+        SharedPreferences spf = getSharedPreferences("spfRecord", MODE_PRIVATE);
+        SharedPreferences.Editor edit = spf.edit();
+        edit.putBoolean("isLogin", false);
+        edit.apply();
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        this.finish();
+    }
+}
